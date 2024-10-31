4487b09c190fed8d52c6281049fb0da2f51c674e
Thomas Harvey <thomasharvey1415@gmail.com>
2024-10-31 01:04:06 +0000

    Angle of enemies is now randomised as well as their x&y speed. Also fixed the bug of enemies slipping out of bounds

--- objects/Enemy.lua
+++ objects/Enemy.lua
@@ -1,13 +1,13 @@
 local love = require "love"
 
 function Enemy(x, y, size, angle)
-    local speed = 100
+  local randomX = math.random(50, 150)
+  local randomY = math.random(50, 150)
     return{
         x = x,
         y = y,
-        angle = angle,
-        x_vel = math.random() * speed * math.cos(angle),
-        y_vel = math.random() * speed * math.sin(angle),
+        x_vel = randomX * math.cos(angle),
+        y_vel = randomY * math.sin(angle),
         radius = size,
 
         draw = function (self)
@@ -20,7 +20,10 @@ function Enemy(x, y, size, angle)
             self.y = self.y + self.y_vel * dt
             -- Enemy bounce off game boundry
             if (self.x < 0 or self.x > 1020 or self.y < 0 or self.y > 780) then
-                -- if enemy hits a boundry then it will go in the opposite direction
+                -- displace the enemy to stop them from going out of bounds, 
+                self.x = self.x - self.x_vel * dt
+                self.y = self.y - self.y_vel * dt
+                -- if enemy hits a boundry then it will go in the opposite direction away from the boundry it bounced off
                 self.x_vel = -self.x_vel
                 self.y_vel = -self.y_vel
             end
